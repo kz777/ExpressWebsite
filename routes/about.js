980fac584584a546007144da057857de5172d163
ASLAN USSAYEV <usprof777@gmail.com>
2016-03-08 12:01:24 -0600

    Templating basic pages

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET About listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('about', { title: 'About'});
 });
 
 module.exports = router;
